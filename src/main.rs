8877cd1fc7a49c3727f8c4d7ccd58b79212fc914
corb1999 <le.corbusier.29845@gmail.com>
2022-08-12 13:28:18 -0400

    cleanup after 13.2

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,7 @@ fn main() {
 
     // scratchwork goes here ...............
 
-    let v1: Vec<i32> = vec![1, 2, 3]; 
-
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect(); 
-
-    assert_eq!(v2, vec![2, 3, 4]); 
-    println!("{:?} ... {:?}", v1, v2);
-
+    
 
 } // end of main function 
 
