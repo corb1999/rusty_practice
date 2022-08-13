0e6e6db61cfc72c2e0abafac840bf6adafe1b555
corb1999 <le.corbusier.29845@gmail.com>
2022-08-13 19:17:33 -0400

    learning rust ch14.1 and 14.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,33 @@
+//! # rusty_practice
+//! 
+//! `rusty_practice` is a non-project, just for goofing and learning rust on my own
+//! 
+//! there is nothing interesting going here. look away
+//! 
 // lib.rs file code below...
 
+
+
+
+
+
+
+
+
+
+
+
+// in lib.rs
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = rusty_practice::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
